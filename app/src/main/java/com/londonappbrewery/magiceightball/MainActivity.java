8926074d414660414a54d3639375756cc9e72f0f
MarAaronHarmony <carter0196753189>
2017-11-05 16:54:18 +0800

    Now able to use adaptive icons

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,11 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //declare the button
+        Button askButton;
+        askButton = (Button) findViewById(R.id.askButton);
+
+        //declare the image holder
+        final ImageView myImage = (ImageView) findViewById(R.id.image_eightBall);
+
+        //declare the array that holds images
+        final int[] images = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random ranGen = new Random();
+                int ran = ranGen.nextInt(4);
+
+                myImage.setImageResource(images[ran]);
+            }
+        });
+
+
+
+
+
+
     }
 }
